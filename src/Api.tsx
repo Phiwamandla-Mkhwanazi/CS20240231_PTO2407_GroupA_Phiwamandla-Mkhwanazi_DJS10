4acfe364449bb4f339dd499a7ce745b4ed4b8f4e
Phiwamandla Mkhwanazi <pm20pm45@gmail.com>
2025-05-14 22:56:05 +0200

    Improved by adding async/await version

--- src/Api.tsx
+++ src/Api.tsx
@@ -1,15 +1,14 @@
-export const data = () => {
-    return fetch('https://jsonplaceholder.typicode.com/posts')
-      .then((response) => {
-        if (!response.ok) {
-          console.error('Data fetching failed');
-          return []; // Return an empty array instead of null
-        }
-        return response.json();
-      })
-      .catch(error => {
-        console.error('Network Error:', error);
-        return []; // Return an empty array in case of a network error
-      });
+export const data = async () => {
+    try {
+      const response = await fetch('https://jsonplaceholder.typicode.com/posts');
+      if (!response.ok) {
+        console.error('Data fetching failed');
+        return [];
+      }
+      return await response.json();
+    } catch (error) {
+      console.error('Network Error:', error);
+      return [];
+    }
   };
   
